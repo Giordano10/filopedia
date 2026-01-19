48bf1ea0e352647d29e9b686b1f5e7d6988ed298
Pedro Gabriel Gabiatti Guardini <pgabiattiguardini@gmail.com>
2026-01-19 19:49:09 -0300

    Testing vercel 2

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+python3 -m pip install -r requirements.txt
+
 echo "Migrando banco de dados..."
 python3 filopedia/manage.py makemigrations --noinput
 python3 filopedia/manage.py migrate --noinput
